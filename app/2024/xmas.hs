f3ebc386e66547c78fedfd5e207624959ead6f88
NicoOhR <nimoohayonr@gmail.com>
2024-12-04 17:06:04 -0600

    struggling

--- app/2024/xmas.hs
+++ app/2024/xmas.hs
@@ -1,26 +1,43 @@
 import Data.List (intercalate, transpose)
-import Data.List.ZigZag (diagonals) -- fucking what
+import qualified Data.Map as Map
+import qualified Data.Set as Set
+import Debug.Trace
 import Text.Regex.TDFA
 import Text.Regex.TDFA.Text ()
+
+type Point = (Int, Int)
+
+makeGrid :: [String] -> Map.Map Point Char
+makeGrid rows =
+  Map.fromList [((r, c), char) | (r, row) <- zip [0 ..] rows, (c, char) <- zip [0 ..] row]
+
+explore :: Point -> Map.Map Point Char -> Bool
+explore p m =
+  case Map.lookup p m of
+    Just 'X' -> findXmas p m
+    _ -> False
+
+findXmas :: Point -> Map.Map Point Char -> Bool
+findXmas start m = loop start "XMAS"
+  where
+    loop _ [] = True
+    loop p (c : cs) =
+      case Map.lookup p m of
+        Just char
+          | char == c ->
+              trace (show p) $
+                any (`loop` cs) (neighbors p)
+        _ -> False
+    neighbors :: Point -> [Point]
+    neighbors (x, y) = [(x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1), (x + 1, y + 1), (x + 1, y - 1), (x - 1, y + 1), (x - 1, y - 1)]
+
+exploreGrid :: Map.Map Point Char -> [Point]
+exploreGrid m = filter (`explore` m) (Map.keys m)
 
 main :: IO ()
 main = do
   contents <- readFile "test.txt"
   let lines_ = lines contents
-  let lineRow = count $ rowWiseLinear lines_
-  let lineCol = count $ rowWiseLinear (transpose lines_)
-  let linDiagR = count (intercalate [] $ diagonals lines_ :: String)
-  let linDiagL = count (intercalate [] $ diagonals (transpose lines_) :: String)
-  mapM_ print $ transpose lines_
-
-count :: String -> Int
-count x = length $ getAllTextMatches (x =~ regex_ :: AllTextMatches [] String)
-  where
-    regex_ = "XMAS|SAMX"
-
--- stupid solution
-rowWiseLinear :: [String] -> String
-rowWiseLinear xs = loop xs ""
-  where
-    loop [] s = s
-    loop (y : ys) s = loop ys s ++ y
+  let grid = makeGrid lines_
+  let matches = exploreGrid grid
+  mapM_ print (matches)
