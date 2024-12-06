c5ee3e58b676d7e521406f6030d7fc1d0fd295a4
NicoOhR <nimoohayonr@gmail.com>
2024-12-05 18:26:15 -0600

    working on the example

--- app/2024/printer.hs
+++ app/2024/printer.hs
@@ -16,10 +16,29 @@ main = do
   let (rules', updates') = (\[x, y] -> (x, y)) $ splitWhen (== "") $ lines file_ -- ghc can cry abt it
   let rules = map readToRule rules'
   let updates = map readToUpdate updates'
-  mapM_ print updates
+  let test = map (`adheres` rules) updates
+  print test
 
 readToRule :: String -> Rule
 readToRule s = (\[x, y] -> (x, y)) $ map read $ splitOn "|" s
 
 readToUpdate :: String -> Update
 readToUpdate s = map read $ splitOn "," s
+
+checkAgainstRule :: Update -> Rule -> Bool
+checkAgainstRule xs (first, second) =
+  loop xs
+  where
+    loop [] = True
+    loop (element : update)
+      | element == second && first `elem` update = False
+      | otherwise = loop update
+
+adheres :: Update -> [Rule] -> Bool
+adheres [] _ = True
+adheres x rs = loop x rs
+  where
+    loop _ [] = True
+    loop update (rule : rules)
+      | checkAgainstRule update rule = loop update rules
+      | otherwise = False
