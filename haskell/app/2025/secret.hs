c5b84d2ae3c9dd87bc6ad2260e85fd7cd8c2316c
Nico OR <nohayonr@gmail.com>
2025-12-01 23:38:40 -0600

    part 2

--- haskell/app/2025/secret.hs
+++ haskell/app/2025/secret.hs
@@ -13,9 +13,26 @@ getPositions = scanl (\x y -> (x + y) `mod` 100) 50
 countZeros :: [Integer] -> Int
 countZeros = length . filter (== 0)
 
+countSwitches :: Integer -> Integer -> Integer
+countSwitches currentPosition turn
+    | newPos <= 0 = abs ((newPos - 1) `div` 100) - if currentPosition == 0 then 1 else 0
+    | otherwise = newPos `div` 100
+  where
+    newPos = currentPosition + turn
+
+totalSwitches :: [Integer] -> Integer
+totalSwitches = snd . foldl step (50, 0)
+  where
+    step :: (Integer, Integer) -> Integer -> (Integer, Integer)
+    step (cur, acc) v =
+        let next = (cur + v) `mod` 100
+         in (next, acc + countSwitches cur v)
+
 main :: IO ()
 main = do
     content <- readFile "input.txt"
     let linesOfFile = lines content
     let zeros = countZeros $ getPositions $ map makeInteger linesOfFile
+    let switches = totalSwitches $ map makeInteger linesOfFile
     print zeros
+    print switches
