f2f760b369beb938acd287587aea43c3d35d3393
Nico OR <nohayonr@gmail.com>
2025-12-04 00:14:27 -0600

    haskell makes life hard sometimes

--- haskell/app/2025/lobby.hs
+++ haskell/app/2025/lobby.hs
@@ -1,11 +1,21 @@
-import Data.Char (digitToInt)
+import Data.Char (digitToInt, intToDigit)
+import Data.Sequence (Seq (Empty), empty, fromList, viewl, viewr)
+import Debug.Trace (traceShowId)
 
 main :: IO ()
 main = do
     content <- lines <$> readFile "input.txt"
     let integers = map (map digitToInt) $ init content
-    let biggest = map partOne integers
-    print $ sum biggest
+    let p1 = map partOne integers
+    let p2 = map partTwo integers
+    print p2
+
+partTwo :: [Int] -> Seq Int
+partTwo list = go list empty :: Seq Int
+  where
+    go :: [Int] -> Seq Int -> Seq Int
+    go [] q = q
+    go (x : xs) q
 
 partOne :: [Int] -> Int
 partOne list = go list 0 0
